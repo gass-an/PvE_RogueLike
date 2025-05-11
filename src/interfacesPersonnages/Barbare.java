d550d261ec2ac1dfb78f318466d47ed043f7b073
Faucon <gass.anthony@gmail.com>
2025-05-12 08:42:21 +1100

    Methodes pour combats + Joueur de base (Paysant) et évolution (Barbare et Magicien)

--- src/interfacesPersonnages/Barbare.java
+++ src/interfacesPersonnages/Barbare.java
@@ -4,6 +4,6 @@ import src.Personnage;
 
 public interface Barbare {
     public void attaqueMelee(Personnage cible);
-    public void paradeDistance(int degatsDistance);
-    public void paradeMelee(int degatsMelee);
+    public void paradeDistance();
+    public void paradeMelee();
 }
